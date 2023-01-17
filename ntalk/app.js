a5f0ff9b680e54f2142f8f0159ba5a092244065f
Diego Neves <pdcassiano@gmail.com>
2023-01-17 18:04:36 -0300

    funcionando

--- ntalk/app.js
+++ ntalk/app.js
@@ -6,10 +6,6 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', routes.index)
-app.get('/usuarios', routes.users.index)
-
-
 
 load('models')
   .then('controllers')
--- ntalk/routes/home.js
+++ ntalk/routes/home.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+// module.exports = router;
 
 module.exports = function (app) {
   var home = app.controllers.home
