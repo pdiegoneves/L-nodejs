1844799dfbba3ae6006711310aeaa2853d847c8c
Diego Neves <pdcassiano@gmail.com>
2023-02-06 10:02:00 -0300

    partials break

--- ntalk/routes/home.js
+++ ntalk/routes/home.js
@@ -1,13 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
-
 module.exports = function (app) {
   var home = app.controllers.home
   app.get('/', home.index)
